967ecb1e5febf6298e9d404d5b271a8a03f7064b
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-12-13 23:36:07 +0100

    feat: try push form data to mongodb

--- src/configForm.js
+++ src/configForm.js
@@ -1,36 +1,37 @@
-let icingColor = document.querySelector("#icingColor").value;
-let toppings = document.querySelector('#toppings').value;
+let icingColor = document.querySelector("#icingColor");
+let toppings = document.querySelector('#toppings');
 
-let customerName = document.querySelector('#customerName').value;
-let customerEmail = document.querySelector('#customerEmail').value;
-let amount = document.querySelector('#amount').value;
-let message = document.querySelector('#message').value;
+let customerName = document.querySelector('[name="name"]').value;
+let customerEmail = document.querySelector('[name=email]').value;
+let amount = document.querySelector('[name=amount]').value;
+let message = document.querySelector('[name=message]').value;
 
 let customerLogo = document.querySelector('#customerLogo').value;
 
-let donutOrder = {
-    donut : {
-        icing : icingColor,
-        topping : toppings,
-        //logo: logo
-    },
-    client : {
-        name : customerName,
-        email : customerEmail,
-    },
-    order : {
-        amount : amount,
-        message : message
-    }
-}
 
-let order = document.querySelector('#orderThisDonut');
-order.addEventListener('click', (e) => {
-    console.log(donutOrder);
+// let donutOrder = {
+//     donut : {
+//         icing : icingColor.value,
+//         topping : toppings.value,
+//         logo: "https://cdn.logo.com/hotlink-ok/logo-social.png"
+//     },
+//     client : {
+//         name : customerName,
+//         email : customerEmail,
+//     },
+//     order : {
+//         amount : amount,
+//         message : message
+//     }
+// }
+
+let donutConfig = document.querySelector('.bakeForm');
+donutConfig.addEventListener('submit', (e) => {
     e.preventDefault();
+    const data = new FormData(e.target);
+    const donutOrder = Object.fromEntries(data.entries());
+    console.log(donutOrder);
     
-    //send to database on https://donutello-backend-n95w.onrender.com/api/v1/donuts/
-
     fetch('https://donutello-backend-n95w.onrender.com/api/v1/donuts/', {
         method: 'POST',
         headers: {
@@ -46,5 +47,4 @@ order.addEventListener('click', (e) => {
         console.error('Error:', error);
     });
 });
-
 
