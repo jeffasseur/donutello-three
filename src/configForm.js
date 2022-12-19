9212ba12f3d81e2e71f0f4938fb8b22698a23224
Rix11-H <60402558+Rix11-H@users.noreply.github.com>
2022-12-19 17:00:30 +0100

    feat: fix formdata to database

--- src/configForm.js
+++ src/configForm.js
@@ -1,37 +1,36 @@
-let icingColor = document.querySelector("#icingColor");
-let toppings = document.querySelector('#toppings');
 
-let customerName = document.querySelector('[name="name"]').value;
-let customerEmail = document.querySelector('[name=email]').value;
-let amount = document.querySelector('[name=amount]').value;
-let message = document.querySelector('[name=message]').value;
-
-let customerLogo = document.querySelector('#customerLogo').value;
-
-
-// let donutOrder = {
-//     donut : {
-//         icing : icingColor.value,
-//         topping : toppings.value,
-//         logo: "https://cdn.logo.com/hotlink-ok/logo-social.png"
-//     },
-//     client : {
-//         name : customerName,
-//         email : customerEmail,
-//     },
-//     order : {
-//         amount : amount,
-//         message : message
-//     }
-// }
-
-let donutConfig = document.querySelector('.bakeForm');
-donutConfig.addEventListener('submit', (e) => {
+document.querySelector('#orderThisDonut').addEventListener('click', (e) => {
     e.preventDefault();
-    const data = new FormData(e.target);
-    const donutOrder = Object.fromEntries(data.entries());
+
+    let icingColor = document.querySelector("#icingColor");
+    let toppings = document.querySelector('#toppings');
+
+    let customerName = document.querySelector('input[name=clientName]').value;
+    let customerEmail = document.querySelector('input[name=clientEmail]').value;
+    let amount = document.querySelector('input[name=amount]').value;
+    let message = document.querySelector('input[name=message]').value;
+
+    let customerLogo = document.querySelector('#customerLogo').value;
+    customerLogo = "https://cdn.logo.com/hotlink-ok/logo-social.png"
+
+    let donutOrder = {
+        donut: {
+            icing: icingColor.value,
+            topping: toppings.value,
+            logo: customerLogo
+        },
+        client: {
+            name: customerName,
+            email: customerEmail,
+        },
+        order: {
+            amount: amount,
+            message: message
+        }
+    }
+
     console.log(donutOrder);
-    
+
     fetch('https://donutello-backend-n95w.onrender.com/api/v1/donuts/', {
         method: 'POST',
         headers: {
@@ -39,12 +38,12 @@ donutConfig.addEventListener('submit', (e) => {
         },
         body: JSON.stringify(donutOrder)
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 });
 
